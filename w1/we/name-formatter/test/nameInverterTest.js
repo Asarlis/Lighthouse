c5ec04583a052fd32e9b197b1e4316ba8e299f68
Sean Day <Asarlis55@gmail.com>
2021-04-18 16:18:52 +0000

    some tests written

--- w1/we/name-formatter/test/nameInverterTest.js
+++ w1/we/name-formatter/test/nameInverterTest.js
@@ -4,5 +4,26 @@ const assert = chai.assert;
 const nameInverter = require('../nameInverter');
 
 describe('nameInverter', function() {
+  it("should return true if an empty string returns an empty string", function (){
+    const empty = "";
+    const result = nameInverter(empty);
+    assert.isTrue(result);
+  });
+  it("should return a single name when given a single name", function(){
+    const single = "name";
+    const result = nameInverter(single);
+    assert.isTrue(result);
+  });
+  it ("should return a single name when passed a single name with extra spaces", function(){
+    const name = "name ";
+    const result = nameInverter(name);
+    assert.isTrue(result);
+  });
+  it ("should return a last-name, first-name when passed a first-name and last-name", function(){
+    const firstLast = "Sean Day";
+    const result = nameInverter(firstLast);
+    assert.isTrue(result);
+  });
+  
 
 });
