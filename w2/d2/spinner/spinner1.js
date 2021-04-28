a3e79cedaca0d8c13d4c34d4616e586ab283394b
Sean Day <Asarlis55@gmail.com>
2021-04-28 00:30:45 +0000

    adding server.js and client.js net intro

---
+++ w2/d2/spinner/spinner1.js
@@ -0,0 +1,22 @@
+setTimeout(() => {
+  process.stdout.wrote('\r|   ');
+}, 100);
+
+setTimeout(() => {
+  process.stdout.wrote('\r/   ');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.wrote('\r-   ');
+}, 500);
+
+setTimeout(() => {
+  process.stdout.wrote('\r\   ');
+}, 700);
+
+setTimeout(() => {
+  process.stdout.wrote('\r|   ');
+}, 900);
+
+
+
